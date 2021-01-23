3adccc5bf5ab1608f46ac2e66767c910fa7a0327
ValentinLHB <valentin.lhb@gmail.com>
2021-01-23 18:17:44 +0100

    added requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', status = False)
+    time = datetime.now().strftime("%H:%M:%S")
+    return render_template('index.html', time = time, status = False)
 
 @app.route('/services')
 def services():
