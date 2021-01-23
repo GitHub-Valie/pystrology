83af8e8f1f9e2d474f2c0681d781d4bd0f1ca8f5
ValentinLHB <valentin.lhb@gmail.com>
2021-01-23 15:38:11 +0100

    fixed navbar

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', status = False)
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
